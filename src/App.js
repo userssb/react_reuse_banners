9838cdf33a8a0dcc47519e4ffc9b606fcd9f6f18
userssb <95171419+userssb@users.noreply.github.com>
2022-12-16 16:28:55 +0530

    updated on 16-12-2022

--- src/App.js
+++ src/App.js
@@ -24,13 +24,49 @@ const bannerCardsList = [
 ]
 
 const App = () => (
-  <div className="banner-container">
-    <ul className="banner-card-list">
-      {bannerCardsList.map(eachCard => (
-        <BannerCardItem bannersList={eachCard} key={eachCard.id} />
-      ))}
-    </ul>
-  </div>
+  <ul>
+    {bannerCardsList.map(eachCard => (
+      <BannerCardItem cardsList={eachCard} key={eachCard.id} />
+    ))}
+  </ul>
 )
 
 export default App
+
+
+// import BannerCardItem from './components/BannerCardItem'
+// import './App.css'
+
+// const bannerCardsList = [
+//   {
+//     id: 1,
+//     headerText: 'The Seasons Latest',
+//     description: 'Get the seasons all latest designs in a flick of your hand',
+//     className: 'card-1',
+//   },
+//   {
+//     id: 2,
+//     headerText: 'Our New Designs',
+//     description:
+//       'Get the designs developed by our in-house team all for yourself',
+//     className: 'card-2',
+//   },
+//   {
+//     id: 3,
+//     headerText: 'Insiders',
+//     description: 'Get the top class products for yourself with an extra off',
+//     className: 'card-3',
+//   },
+// ]
+
+// const App = () => (
+//   <div className="banner-container">
+//     <ul className="banner-card-list">
+//       {bannerCardsList.map(eachCard => (
+//         <BannerCardItem bannersList={eachCard} key={eachCard.id} />
+//       ))}
+//     </ul>
+//   </div>
+// )
+
+// export default App
