91c6ce09b59b454bfeae5f3e2af0f6876a821d64
userssb <95171419+userssb@users.noreply.github.com>
2022-12-16 16:26:04 +0530

    updated on 16-12-2022

--- src/components/BannerCardItem/index.js
+++ src/components/BannerCardItem/index.js
@@ -1,18 +1,38 @@
 import './index.css'
 
 const BannerCardItem = props => {
-  const {bannersList} = props
-  const {headerText, description, className} = bannersList
+  const {cardsList} = props
+  const {id, headerText, description, className} = cardsList
   return (
-    <li className={`${className} banner-card-item`}>
-      <div>
-        <h1>{headerText}</h1>
-        <p>{description}</p>
-        <button className="show-more-button" type="button">
-          Show More
-        </button>
+    <li className="list-cont">
+      <div className={className}>
+        <div className="text-cont">
+          <h1>{headerText}</h1>
+          <p>{description}</p>
+          <button className="butt">Show More</button>
+        </div>
       </div>
     </li>
   )
 }
 export default BannerCardItem
+
+
+// import './index.css'
+
+// const BannerCardItem = props => {
+//   const {bannersList} = props
+//   const {headerText, description, className} = bannersList
+//   return (
+//     <li className={`${className} banner-card-item`}>
+//       <div>
+//         <h1>{headerText}</h1>
+//         <p>{description}</p>
+//         <button className="show-more-button" type="button">
+//           Show More
+//         </button>
+//       </div>
+//     </li>
+//   )
+// }
+// export default BannerCardItem
